dda433025463bb7d75e4c5856cb5e5d9cd2bf575
tatsu0906 <ttnr0906@gmail.com>
2018-01-17 03:29:25 +0900

    Make site django base

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,20 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.post_list, name='post_list'),
+	#front page
+    url(r'^$', views.render_index, name='index'),
+    url(r'^7$', views.render_7, name='7'),
+    url(r'^matrix$', views.render_matrix, name='matrix'),
+    url(r'^title$', views.render_title, name='title'),
+	url(r'^24$', views.render_24, name='24'),
+	url(r'^17$', views.render_17, name='17'),
+	url(r'^coreterm$', views.render_coreterm, name='coreterm'),
+	url(r'^suppleterm$', views.render_suppleterm, name='suppleterm'),
+	url(r'^board$', views.render_board, name='board'),
+
+
+
+    #url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
+    #url(r'^post/new/$', views.post_new, name='post_new'),
+    #url(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,74 @@
-from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
+from django.shortcuts import render, get_object_or_404, redirect
+from .forms import PostForm
 
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    #posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    #return render(request, 'blog/post_list.html', {'posts': posts})
     return render(request, 'blog/post_list.html', {})
+
+def render_index(request):
+    return render(request, 'blog/index.html', {})
+
+def render_7(request):
+    return render(request, 'blog/7.html', {})
+
+def render_matrix(request):
+    return render(request, 'blog/matrix.html', {})
+
+def render_title(request):
+    return render(request, 'blog/title.html', {})
+
+def render_24(request):
+    return render(request, 'blog/24.html', {})
+
+def render_17(request):
+    return render(request, 'blog/17.html', {})    
+
+def render_coreterm(request):
+    return render(request, 'blog/coreterm.html', {})    
+
+def render_suppleterm(request):
+    return render(request, 'blog/suppleterm.html', {})    
+
+def render_board(request):
+    return render(request, 'blog/board.html', {})    
+
+
+
+
+
+#########################################################################
+"""
+def post_detail(request, pk):
+	post = get_object_or_404(Post, pk=pk)
+	return render(request, 'blog/post_detail.html', {'post' : post})
+
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_edit.html', {'form': form})
+"""
